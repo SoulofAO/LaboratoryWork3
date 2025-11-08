e8f03084d431bb176e1e0c6f2f6763cc3d0d0f7b
SoulofAO <SoulofAO@mail.com>
2025-11-08 12:13:25 +0300

    sfsf

--- LaboratoryWork3.cpp
+++ LaboratoryWork3.cpp
@@ -52,43 +52,61 @@ T* median_of_three(T* a, T* b, T* c, Compare comp) {
 }
 
 template<typename T, typename Compare>
-void sort(T* first, T* last, Compare comp) {
-    if (first == nullptr || last == nullptr) return;
-    const std::ptrdiff_t INSERTION_THRESHOLD = 15;
-
-    while (last - first > static_cast<std::ptrdiff_t>(INSERTION_THRESHOLD)) {
-        T* mid = first + (last - first - 1) / 2;
-        T* piv_ptr = median_of_three(first, mid, last - 1, comp);
-        if (piv_ptr != last - 1) move_swap(*piv_ptr, *(last - 1));
-        T pivot = std::move(*(last - 1));
-
-        T* i = first;
-        for (T* j = first; j < last - 1; ++j) {
-            if (comp(*j, pivot)) {
-                if (i != j) move_swap(*i, *j);
-                ++i;
-            }
-        }
-        *i = std::move(pivot);
-
-        T* partition = i;
-        auto left_size = partition - first;
-        auto right_size = last - (partition + 1);
-
-        if (left_size < right_size) {
-            if (left_size > 0) sort(first, partition, comp);
-            first = partition + 1;
-        }
-        else {
-            if (right_size > 0) sort(partition + 1, last, comp);
-            last = partition;
-        }
+void sort(T* First, T* Last, Compare comp) {
+    if (First == nullptr || Last == nullptr) {
+        return;
     }
 
-    insertion_sort(first, last, comp);
+    const std::ptrdiff_t INSERTION_THRESHOLD = 16;
+
+    while (Last - First > static_cast<std::ptrdiff_t>(INSERTION_THRESHOLD)) {
+        T* Mid = First + (Last - First - 1) / 2;
+        T* PivotPtr = median_of_three(First, Mid, Last - 1, comp);
+        T PivotValue = *PivotPtr; 
+
+        T* I = First - 1;
+        T* J = Last;
+        while (true) {
+            do {
+                ++I;
+            } while (comp(*I, PivotValue));
+
+            do {
+                --J;
+            } while (comp(PivotValue, *J));
+
+            if (I >= J) {
+                break;
+            }
+
+            if (I != J) {
+                move_swap(*I, *J);
+            }
+        }
+
+        T* Partition = J;
+        auto LeftSize = Partition - First + 1;     
+        auto RightSize = Last - (Partition + 1);   
+
+        if (LeftSize < RightSize) {
+            if (LeftSize > 0) {
+                sort(First, Partition + 1, comp);
+            }
+            First = Partition + 1;
+        }
+        else {
+            if (RightSize > 0) {
+                sort(Partition + 1, Last, comp);
+            }
+            Last = Partition + 1;
+        }
+    }
+    insertion_sort(First, Last, comp);
 }
 
 
+
+//Измерить время сортировки вставками и Qiucksort. Построить графики. Худшие случаи. 
 template<typename T, typename Compare>
 bool is_sorted_by(T* first, T* last, Compare comp) {
     if (first == last) return true;
